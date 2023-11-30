c8c228b1b9f66c42f6614734130ebdaa1851cf8a
Duke <dukemorise33@gmail.com>
2023-11-29 22:53:32 -0800

    completed lesson

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,35 @@
+import random
+
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    random_list = []
+    for i in range(0,4):
+        random_list.append(random.randrange(0,100))
+    return random_list
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, element)
+    return l
 
 def remove_element_from_end_of_list(l):
-    return None
+    l.pop()
+    return l
 
 def remove_element_from_start_of_list(l):
-    return None
+    l.pop(0)
+    return l
 
 def retrieve_first_element_from_list(l):
-    return None
+    return l[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+    return l[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+    return l[-1]
